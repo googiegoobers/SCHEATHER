7cb41c009dcb589f47a3854af26d3b4a7b1078ab
mantaurmantaur <orng.digital@gmail.com>
2025-06-23 07:07:54 +0800

    [Update][Bautista]-adjust-landing-for-mobile

--- scheather-next-app/app/dashboard/page.tsx
+++ scheather-next-app/app/dashboard/page.tsx
@@ -130,6 +130,7 @@ export default function Dashboard() {
               Scheather
             </div>
           </div>
+          {/* //notification and avatar section */}
           <div className="relative flex items-center gap-2 sm:gap-4">
             <Link href="#" passHref>
               <svg
@@ -141,6 +142,7 @@ export default function Dashboard() {
               >
                 <path d="M17.133 12.632v-1.8a5.406 5.406 0 0 0-4.154-5.262.955.955 0 0 0 .021-.106V3.1a1 1 0 0 0-2 0v2.364a.955.955 0 0 0 .021.106 5.406 5.406 0 0 0-4.154 5.262v1.8C6.867 15.018 5 15.614 5 16.807 5 17.4 5 18 5.538 18h12.924C19 18 19 17.4 19 16.807c0-1.193-1.867-1.789-1.867-4.175ZM8.823 19a3.453 3.453 0 0 0 6.354 0H8.823Z" />
               </svg>
+<<<<<<< Updated upstream
             </Link>
             {/* Profile image (always shown) */}
             <img
@@ -148,6 +150,7 @@ export default function Dashboard() {
               src="https://placehold.co/65x65"
               alt="Profile"
             />
+=======
 
             {/* Desktop name + email */}
             <div className="hidden sm:flex flex-col min-w-0 truncate">

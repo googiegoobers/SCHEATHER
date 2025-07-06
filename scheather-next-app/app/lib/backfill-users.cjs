c4a4a4e33eae59d30a80f118d049385e68046c6e
Elle Gabrielle Bernante <ellegabriellebernante@gmail.com>
2025-07-06 23:27:49 +0800

    Remove import assertion from serviceAccount import
    The import assertion for the serviceAccountKey.json file was removed to improve compatibility or resolve issues with the import statement.

--- scheather-next-app/app/lib/backfill-users.cjs
+++ scheather-next-app/app/lib/backfill-users.cjs
@@ -1,6 +1,6 @@
 // backfill-users.js
 import admin from "firebase-admin";
-import serviceAccount from "./serviceAccountKey.json" assert { type: "json" };
+import serviceAccount from "./serviceAccountKey.json"
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),

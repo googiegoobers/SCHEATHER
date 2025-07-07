4beb6bf6d6fb7bc5655b71ea0bb4579046ec1414
Kawrr <kaawwrr@gmail.com>
2025-07-08 01:45:52 +0800

    [update][abella] click event

--- scheather-next-app/app/components/Calendar.tsx
+++ scheather-next-app/app/components/Calendar.tsx
@@ -151,6 +151,12 @@ const CalendarComponent: React.FC = () => {
   const handleSelectEvent = (evt: CalendarEvent) =>
     setSelectedEvent(evt as unknown as FirestoreEvent);
 
+  function toLocalDateTimeInputValue(date: Date) {
+  const offset = date.getTimezoneOffset();
+  const local = new Date(date.getTime() - offset * 60 * 1000);
+  return local.toISOString().slice(0, 16);
+}
+
   /* ---------- Render ---------- */
 
   return (

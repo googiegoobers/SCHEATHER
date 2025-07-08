89b373273fbbf03d5bfa50d3a135bb8bef238cea
goofygoober <villamilrhona@gmail.com>
2025-07-08 17:26:48 +0800

    Update HamburgerCal.tsx
    -changed to @/lib/firebaseConfig

--- scheather-next-app/app/components/HamburgerCal.tsx
+++ scheather-next-app/app/components/HamburgerCal.tsx
@@ -1,8 +1,7 @@
-
-import { useState, useEffect } from 'react';
-import Calendar from 'react-calendar';
-import "./HamburgerCal.css"
-import { auth, db } from "@/app/lib/firebaseConfig";
+import { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+import "./HamburgerCal.css";
+import { auth, db } from "@/lib/firebaseConfig";
 import {
   collection,
   getDocs,
@@ -37,15 +36,17 @@ interface FirestoreEvent {
 
 function HamburgerCal() {
   const [value, onChange] = useState<Value>(new Date());
-  
+
   const [date, setDate] = useState(new Date());
   const [events, setEvents] = useState<FirestoreEvent[]>([]);
   const [currentUser, setCurrentUser] = useState<any>(null);
   const today = new Date();
-  
+
   // Auth listener
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => setCurrentUser(user));
+    const unsubscribe = onAuthStateChanged(auth, (user) =>
+      setCurrentUser(user)
+    );
     return () => unsubscribe();
   }, []);
 
@@ -107,7 +108,7 @@ function HamburgerCal() {
 
     fetchEvents();
   }, [currentUser]);
-  
+
   const hasEventOnDate = (date: Date) => {
     return events.some(
       (event) => new Date(event.start).toDateString() === date.toDateString()
@@ -115,33 +116,37 @@ function HamburgerCal() {
   };
 
   return (
-    <div data-layer="the body" className=" bg-[#94B7EF] rounded p-4 lg:w-[18vw] lg:h-[42vh] xl:w-[12vw] xl:h-[28vh]">
-      <Calendar 
-      onChange={onChange} 
-      value={value}
-      calendarType= "gregory"
-      showNeighboringCentury={false}
-      showNeighboringDecade={false}
-      formatShortWeekday={(locale, date) => date.toLocaleDateString(locale, {weekday: 'narrow'})}
-      
-      //if naay events within that day mo gawas siya ug orange circle
-      tileContent={({ date, view }) => 
-            view === "month" && hasEventOnDate(date) ? (
+    <div
+      data-layer="the body"
+      className=" bg-[#94B7EF] rounded p-4 lg:w-[18vw] lg:h-[42vh] xl:w-[12vw] xl:h-[28vh]"
+    >
+      <Calendar
+        onChange={onChange}
+        value={value}
+        calendarType="gregory"
+        showNeighboringCentury={false}
+        showNeighboringDecade={false}
+        formatShortWeekday={(locale, date) =>
+          date.toLocaleDateString(locale, { weekday: "narrow" })
+        }
+        //if naay events within that day mo gawas siya ug orange circle
+        tileContent={({ date, view }) =>
+          view === "month" && hasEventOnDate(date) ? (
             <div className="event-indicator" />
-            ) : null
+          ) : null
         }
-      tileClassName={({ date, view }) => {
-        if (
-          date.getDate() === today.getDate() &&
-          date.getMonth() === today.getMonth() &&
-          date.getFullYear() === today.getFullYear()
-           ) {
-          return "today-text-color";
-        }
-      }}
-        />
+        tileClassName={({ date, view }) => {
+          if (
+            date.getDate() === today.getDate() &&
+            date.getMonth() === today.getMonth() &&
+            date.getFullYear() === today.getFullYear()
+          ) {
+            return "today-text-color";
+          }
+        }}
+      />
     </div>
   );
-};
+}
 
 export default HamburgerCal;
